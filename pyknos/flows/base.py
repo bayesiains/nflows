9f9fb3fcccbbd2de9801b60e34541a0a2044ed3c
Michael Deistler <Michael.deistler@t-online.de>
2020-02-13 17:28:05 +0100

    embedding_net now in pyknos

--- pyknos/flows/base.py
+++ pyknos/flows/base.py
@@ -2,39 +2,51 @@
 
 from pyknos.distributions.base import Distribution
 from pyknos.utils import torchutils
-
+import torch.nn as nn
 
 class Flow(Distribution):
     """Base class for all flow objects."""
 
-    def __init__(self, transform, distribution):
+    def __init__(self, transform, distribution, embedding_net=None):
         """Constructor.
 
         Args:
             transform: A `Transform` object, it transforms data into noise.
             distribution: A `Distribution` object, the base distribution of the flow that
                 generates the noise.
+            embedding_net: A `nn.Module` which has trainable parameters to encode the
+                context (condition). It is trained jointly with the flow.
         """
         super().__init__()
         self._transform = transform
         self._distribution = distribution
+        if embedding_net is not None:
+            assert isinstance(embedding_net, nn.Module), 'embedding_net is not a nn.Module. ' \
+                                                         'If you want to use hard-coded summary features, ' \
+                                                         'please simply pass the encoded features and pass ' \
+                                                         'embedding_net=None'
+            self._embedding_net = embedding_net
+        else:
+            self._embedding_net = nn.Identity()
 
     def _log_prob(self, inputs, context):
-        noise, logabsdet = self._transform(inputs, context=context)
-        log_prob = self._distribution.log_prob(noise, context=context)
+        embedded_context = self._embedding_net(context)
+        noise, logabsdet = self._transform(inputs, context=embedded_context)
+        log_prob = self._distribution.log_prob(noise, context=embedded_context)
         return log_prob + logabsdet
 
     def _sample(self, num_samples, context):
-        noise = self._distribution.sample(num_samples, context=context)
+        embedded_context = self._embedding_net(context)
+        noise = self._distribution.sample(num_samples, context=embedded_context)
 
-        if context is not None:
+        if embedded_context is not None:
             # Merge the context dimension with sample dimension in order to apply the transform.
             noise = torchutils.merge_leading_dims(noise, num_dims=2)
-            context = torchutils.repeat_rows(context, num_reps=num_samples)
+            embedded_context = torchutils.repeat_rows(embedded_context, num_reps=num_samples)
 
-        samples, _ = self._transform.inverse(noise, context=context)
+        samples, _ = self._transform.inverse(noise, context=embedded_context)
 
-        if context is not None:
+        if embedded_context is not None:
             # Split the context dimension from sample dimension.
             samples = torchutils.split_leading_dim(samples, shape=[-1, num_samples])
 
@@ -45,18 +57,19 @@ class Flow(Distribution):
 
         For flows, this is more efficient that calling `sample` and `log_prob` separately.
         """
+        embedded_context = self._embedding_net(context)
         noise, log_prob = self._distribution.sample_and_log_prob(
-            num_samples, context=context
+            num_samples, context=embedded_context
         )
 
-        if context is not None:
+        if embedded_context is not None:
             # Merge the context dimension with sample dimension in order to apply the transform.
             noise = torchutils.merge_leading_dims(noise, num_dims=2)
-            context = torchutils.repeat_rows(context, num_reps=num_samples)
+            embedded_context = torchutils.repeat_rows(embedded_context, num_reps=num_samples)
 
-        samples, logabsdet = self._transform.inverse(noise, context=context)
+        samples, logabsdet = self._transform.inverse(noise, context=embedded_context)
 
-        if context is not None:
+        if embedded_context is not None:
             # Split the context dimension from sample dimension.
             samples = torchutils.split_leading_dim(samples, shape=[-1, num_samples])
             logabsdet = torchutils.split_leading_dim(logabsdet, shape=[-1, num_samples])
@@ -74,5 +87,5 @@ class Flow(Distribution):
         Returns:
             A `Tensor` of shape [batch_size, ...], the noise.
         """
-        noise, _ = self._transform(inputs, context=context)
+        noise, _ = self._transform(inputs, context=self._embedding_net(context))
         return noise
