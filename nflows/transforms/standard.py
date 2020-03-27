5d76e164bddc5d032384ad6ec0c1b7abd20ff91b
Alvaro Tejero-Cantero <alvaro.tejero@tum.de>
2020-03-27 15:25:03 +0100

    New name and defaults API.
    Shift and scale now receive defaults visible in the signature
    
    Classes added for compatibility still accept shift/scale = None.

--- nflows/transforms/standard.py
+++ nflows/transforms/standard.py
@@ -20,10 +20,12 @@ class IdentityTransform(Transform):
         return self(inputs, context)
 
 
-class AffineScalarTransform(Transform):
-    """Computes X = X * scale + shift, where scale and shift are scalars, and scale is non-zero."""
-
-    def __init__(self, shift=None, scale=None):
+class PointwiseAffineTransform(Transform):
+    def __init__(
+        self,
+        shift: torch.Tensor = torch.tensor(0.0),
+        scale: torch.Tensor = torch.tensor(1.0),
+    ):
         super().__init__()
 
         if shift is None and scale is None:
@@ -57,20 +59,23 @@ class AffineScalarTransform(Transform):
         return outputs, logabsdet
 
 
-class AffineTransform(Transform):
-    def __init__(self, shift=None, scale=None):
-        super().__init__()
+class AffineTransform(PointwiseAffineTransform):
+    def __init__(
+        self,
+        shift: torch.Tensor = torch.tensor(0.0),
+        scale: torch.Tensor = torch.tensor(1.0),
+    ):
 
-        self.register_buffer(
-            "_shift", ensure_tensor(shift if (shift is not None) else 0.0)
-        )
-        self.register_buffer(
-            "_scale", ensure_tensor(scale if (scale is not None) else 1.0)
-        )
+        warnings.warn("Use PointwiseAffineTransform", DeprecationWarning)
 
-    @property
-    def _log_scale(self):
-        return torch.log(torch.abs(self._scale))
+        if shift is None:
+            warnings.warn("`shift=None` deprecated; default is 0.0")
+            shift = torch.tensor(0.0)
+        if scale is None:
+            warnings.warn("`scale=None` deprecated; default is 1.0.")
+            scale = torch.tensor(1.0)
+
+        super().__init__(shift, scale)
 
     def forward(self, inputs, context=None):
         batch_size = inputs.shape[0]
@@ -78,8 +83,4 @@ class AffineTransform(Transform):
         logabsdet = self._log_scale.reshape(1, -1).repeat(batch_size, 1).sum(dim=-1)
         return outputs, logabsdet
 
-    def inverse(self, inputs, context=None):
-        batch_size = inputs.shape[0]
-        outputs = (inputs - self._shift) / self._scale
-        logabsdet = -self._log_scale.reshape(1, -1).repeat(batch_size, 1).sum(dim=-1)
-        return outputs, logabsdet
+AffineScalarTransform = AffineTransform
