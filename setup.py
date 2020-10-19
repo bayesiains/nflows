3a551ef08b096f1be26c04e10483fc6c759f30e2
arturbekasov <artur.bekasov@gmail.com>
2020-10-19 21:10:05 +0100

    Use README.md as package description.

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
+from os import path
 from setuptools import find_packages, setup
 
 exec(open("nflows/version.py").read())
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="nflows",
     version=__version__,
     description="Normalizing flows in PyTorch.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/bayesiains/nflows",
     download_url = 'https://github.com/bayesiains/nflows/archive/v0.12.tar.gz',
     author="Conor Durkan, Artur Bekasov, George Papamakarios, Iain Murray",
