3ffd42b06cad6a24b4aef51b773a5e977942876f
Johann Brehmer <mail@johannbrehmer.de>
2020-10-28 13:37:46 -0400

    Added tests for spline transforms with spline-only inputs

--- tests/transforms/splines/cubic_test.py
+++ tests/transforms/splines/cubic_test.py
@@ -60,3 +60,32 @@ class UnconstrainedCubicSplineTest(torchtestcase.TorchTestCase):
         self.eps = 1e-4
         self.assertEqual(inputs, inputs_inv)
         self.assertEqual(logabsdet + logabsdet_inv, torch.zeros_like(logabsdet))
+
+    def test_forward_inverse_are_consistent_in_tails(self):
+        num_bins = 10
+        shape = [2, 3, 4]
+        tail_bound = 1.0
+
+        unnormalized_widths = torch.randn(*shape, num_bins)
+        unnormalized_heights = torch.randn(*shape, num_bins)
+        unnorm_derivatives_left = torch.randn(*shape, 1)
+        unnorm_derivatives_right = torch.randn(*shape, 1)
+
+        def call_spline_fn(inputs, inverse=False):
+            return splines.unconstrained_cubic_spline(
+                inputs=inputs,
+                unnormalized_widths=unnormalized_widths,
+                unnormalized_heights=unnormalized_heights,
+                unnorm_derivatives_left=unnorm_derivatives_left,
+                unnorm_derivatives_right=unnorm_derivatives_right,
+                inverse=inverse,
+                tail_bound=tail_bound
+            )
+
+        inputs = torch.sign(torch.randn(*shape)) * (tail_bound + torch.rand(*shape))  # Now *all* inputs are outside [-tail_bound, tail_bound].
+        outputs, logabsdet = call_spline_fn(inputs, inverse=False)
+        inputs_inv, logabsdet_inv = call_spline_fn(outputs, inverse=True)
+
+        self.eps = 1e-4
+        self.assertEqual(inputs, inputs_inv)
+        self.assertEqual(logabsdet + logabsdet_inv, torch.zeros_like(logabsdet))
