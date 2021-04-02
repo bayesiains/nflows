bcb6dd4dbb973980079ca64bdcebd2bc8249ed73
Patryk Wielopolski <patryk.wielopolski@datawalk.com>
2021-04-02 13:38:57 +0200

    Conditional Masked Autoregressive Flow implementation.

--- nflows/flows/autoregressive.py
+++ nflows/flows/autoregressive.py
@@ -1,8 +1,9 @@
 """Implementations of autoregressive flows."""
 
-from torch.nn import functional as F
+import torch.nn as nn
+import torch.nn.functional as F
 
-from nflows.distributions.normal import StandardNormal
+from nflows.distributions.normal import ConditionalDiagonalNormal, StandardNormal
 from nflows.flows.base import Flow
 from nflows.transforms.autoregressive import MaskedAffineAutoregressiveTransform
 from nflows.transforms.base import CompositeTransform
@@ -19,18 +20,18 @@ class MaskedAutoregressiveFlow(Flow):
     """
 
     def __init__(
-        self,
-        features,
-        hidden_features,
-        num_layers,
-        num_blocks_per_layer,
-        use_residual_blocks=True,
-        use_random_masks=False,
-        use_random_permutations=False,
-        activation=F.relu,
-        dropout_probability=0.0,
-        batch_norm_within_layers=False,
-        batch_norm_between_layers=False,
+            self,
+            features,
+            hidden_features,
+            num_layers,
+            num_blocks_per_layer,
+            use_residual_blocks=True,
+            use_random_masks=False,
+            use_random_permutations=False,
+            activation=F.relu,
+            dropout_probability=0.0,
+            batch_norm_within_layers=False,
+            batch_norm_between_layers=False,
     ):
 
         if use_random_permutations:
@@ -59,4 +60,58 @@ class MaskedAutoregressiveFlow(Flow):
         super().__init__(
             transform=CompositeTransform(layers),
             distribution=StandardNormal([features]),
+        )
+
+
+class ConditionalMaskedAutoregressiveFlow(Flow):
+    """An Conditional autoregressive flow that uses affine transforms with masking.
+
+    Reference:
+    > G. Papamakarios et al., Masked Autoregressive Flow for Density Estimation,
+    > Advances in Neural Information Processing Systems, 2017.
+    """
+
+    def __init__(
+            self,
+            features,
+            hidden_features,
+            context_features,
+            num_layers=5,
+            num_blocks_per_layer=2,
+            use_residual_blocks=True,
+            use_random_masks=False,
+            use_random_permutations=False,
+            activation=F.relu,
+            dropout_probability=0.0,
+            batch_norm_within_layers=False,
+            batch_norm_between_layers=False,
+    ):
+
+        if use_random_permutations:
+            permutation_constructor = RandomPermutation
+        else:
+            permutation_constructor = ReversePermutation
+
+        layers = []
+        for _ in range(num_layers):
+            layers.append(permutation_constructor(features))
+            layers.append(
+                MaskedAffineAutoregressiveTransform(
+                    features=features,
+                    hidden_features=hidden_features,
+                    context_features=context_features,
+                    num_blocks=num_blocks_per_layer,
+                    use_residual_blocks=use_residual_blocks,
+                    random_mask=use_random_masks,
+                    activation=activation,
+                    dropout_probability=dropout_probability,
+                    use_batch_norm=batch_norm_within_layers,
+                )
+            )
+            if batch_norm_between_layers:
+                layers.append(BatchNorm(features))
+
+        super().__init__(
+            transform=CompositeTransform(layers),
+            distribution=StandardNormal([features])
         )
--- tests/flows/autoregressive_test.py
+++ tests/flows/autoregressive_test.py
@@ -31,5 +31,34 @@ class MaskedAutoregressiveFlowTest(torchtestcase.TorchTestCase):
         self.assertEqual(samples.shape, torch.Size([num_samples, features]))
 
 
+class ConditionalMaskedAutoregressiveFlowTest(torchtestcase.TorchTestCase):
+    def test_log_prob(self):
+        batch_size = 10
+        features = 20
+        context_features = 5
+        flow = ar.ConditionalMaskedAutoregressiveFlow(
+            features=features, hidden_features=30, context_features=context_features
+        )
+        inputs = torch.randn(batch_size, features)
+        context = torch.randn(batch_size, context_features)
+        log_prob = flow.log_prob(inputs, context)
+        self.assertIsInstance(log_prob, torch.Tensor)
+        self.assertEqual(log_prob.shape, torch.Size([batch_size]))
+
+    def test_sample(self):
+        num_samples = 10
+
+        batch_size = 10
+        features = 20
+        context_features = 5
+        flow = ar.ConditionalMaskedAutoregressiveFlow(
+            features=features, hidden_features=30, context_features=context_features
+        )
+        context = torch.randn(batch_size, context_features)
+        samples = flow.sample(num_samples, context)
+        self.assertIsInstance(samples, torch.Tensor)
+        self.assertEqual(samples.shape, torch.Size([batch_size, num_samples, features]))
+
+
 if __name__ == "__main__":
     unittest.main()
