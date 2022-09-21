41bd2b8dffa393dd38b45c971afeb1d414360ad4
Nathan Simpson <email.nsimpson@gmail.com>
2022-09-21 10:18:08 +0200

    Update base.py

--- nflows/distributions/base.py
+++ nflows/distributions/base.py
@@ -48,15 +48,17 @@ class Distribution(nn.Module):
         Args:
             num_samples: int, number of samples to generate.
             context: Tensor or None, conditioning variables. If None, the context is ignored. 
-                     Should have shape [context_size, context_features], which will generate
-                     num_samples for each context item provided. The overall shape of the samples 
-                     will then be [context_size, num_samples, features].
+                     Should have shape [context_size, ...], where ... represents a (context) feature 
+                     vector of arbitrary shape. This will generate num_samples for each context item 
+                     provided. The overall shape of the samples will then be 
+                     [context_size, num_samples, ...].
             batch_size: int or None, number of samples per batch. If None, all samples are generated
                 in one batch.
 
         Returns:
-            A Tensor containing the samples, with shape [num_samples, features] if context is None, or
-            [context_size, num_samples, features] if context is given.
+            A Tensor containing the samples, with shape [num_samples, ...] if context is None, or
+            [context_size, num_samples, ...] if context is given, where ... represents a feature
+            vector of arbitrary shape.
         """
         if not check.is_positive_int(num_samples):
             raise TypeError("Number of samples must be a positive integer.")
