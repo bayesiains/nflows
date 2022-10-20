18edbcf15beb437c17cfe3ead77e52218ecfa0f1
Michele Invernizzi <inve.michele@gmail.com>
2022-10-20 10:01:03 +0200

    added test for identity init

--- tests/transforms/splines/rational_quadratic_test.py
+++ tests/transforms/splines/rational_quadratic_test.py
@@ -30,6 +30,36 @@ class RationalQuadraticSplineTest(torchtestcase.TorchTestCase):
         self.assertEqual(inputs, inputs_inv)
         self.assertEqual(logabsdet + logabsdet_inv, torch.zeros_like(logabsdet))
 
+    def test_identity_init(self):
+        num_bins = 10
+        shape = [2, 3, 4]
+
+        unnormalized_widths = torch.zeros(*shape, num_bins)
+        unnormalized_heights = torch.zeros(*shape, num_bins)
+        unnormalized_derivatives = torch.zeros(*shape, num_bins + 1)
+
+        def call_spline_fn(inputs, inverse=False):
+            return splines.rational_quadratic_spline(
+                inputs=inputs,
+                unnormalized_widths=unnormalized_widths,
+                unnormalized_heights=unnormalized_heights,
+                unnormalized_derivatives=unnormalized_derivatives,
+                inverse=inverse,
+                allow_identity_init=True,
+            )
+
+        inputs = torch.rand(*shape)
+        outputs, logabsdet = call_spline_fn(inputs, inverse=False)
+
+        self.eps = 1e-6
+        self.assertEqual(inputs, outputs)
+        self.assertEqual(logabsdet, torch.zeros_like(logabsdet))
+
+        inputs = torch.rand(*shape)
+        outputs, logabsdet = call_spline_fn(inputs, inverse=True)
+
+        self.assertEqual(inputs, outputs)
+        self.assertEqual(logabsdet, torch.zeros_like(logabsdet))
 
 class UnconstrainedRationalQuadraticSplineTest(torchtestcase.TorchTestCase):
     def test_forward_inverse_are_consistent(self):
@@ -82,3 +112,35 @@ class UnconstrainedRationalQuadraticSplineTest(torchtestcase.TorchTestCase):
         self.eps = 1e-4
         self.assertEqual(inputs, inputs_inv)
         self.assertEqual(logabsdet + logabsdet_inv, torch.zeros_like(logabsdet))
+
+    def test_identity_init(self):
+        num_bins = 10
+        shape = [2, 3, 4]
+        tail_bound = 1.0
+
+        unnormalized_widths = torch.zeros(*shape, num_bins)
+        unnormalized_heights = torch.zeros(*shape, num_bins)
+        unnormalized_derivatives = torch.zeros(*shape, num_bins + 1)
+
+        def call_spline_fn(inputs, inverse=False):
+            return splines.unconstrained_rational_quadratic_spline(
+                inputs=inputs,
+                unnormalized_widths=unnormalized_widths,
+                unnormalized_heights=unnormalized_heights,
+                unnormalized_derivatives=unnormalized_derivatives,
+                inverse=inverse,
+                allow_identity_init=True,
+            )
+
+        inputs = torch.sign(torch.randn(*shape)) * (tail_bound + torch.rand(*shape))  # Now *all* inputs are outside [-tail_bound, tail_bound].
+        outputs, logabsdet = call_spline_fn(inputs, inverse=False)
+
+        self.eps = 1e-6
+        self.assertEqual(inputs, outputs)
+        self.assertEqual(logabsdet, torch.zeros_like(logabsdet))
+
+        inputs = torch.rand(*shape)
+        outputs, logabsdet = call_spline_fn(inputs, inverse=True)
+
+        self.assertEqual(inputs, outputs)
+        self.assertEqual(logabsdet, torch.zeros_like(logabsdet))
